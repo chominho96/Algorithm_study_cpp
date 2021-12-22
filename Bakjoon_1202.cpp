447bb5f499f34ddf4be08b157d6e1af922a99fc1
CHO <chominho96@naver.com>
2021-12-22 13:27:38 +0900

    edit Bakjoon_1202

--- Bakjoon_1202.cpp
+++ Bakjoon_1202.cpp
@@ -9,16 +9,6 @@ struct wei_val {
 	int value;
 };
 
-//struct cmp {
-//	bool operator()(const wei_val& w_v_1, const wei_val& w_v_2) {
-//		if (w_v_1.weight == w_v_2.weight) {
-//			return w_v_1.value < w_v_2.value;
-//		}
-//		else {
-//			return w_v_1.weight > w_v_2.weight;
-//		}
-//	}
-//};
 
 struct cmp {
 	bool operator()(const wei_val& w_v_1, const wei_val& w_v_2) {
@@ -35,7 +25,6 @@ int main() {
 	cin >> N >> K;
 
 	priority_queue<wei_val, vector<wei_val>, cmp> jewerly;
-	//priority_queue<int, vector<int>, greater<int>> bag;
 	multiset<int> bag;
 
 	int M, V;
@@ -66,33 +55,6 @@ int main() {
 	}
 
 	cout << result << endl;
-
-
-
-	/*jewerly.push(wei_val{ 10, 4 });
-	jewerly.push(wei_val{ 1, 3 });
-	jewerly.push(wei_val{ 1, 4 });
-	jewerly.push(wei_val{ 3, 5 });
-	jewerly.push(wei_val{ 3, 4 });
-	cout << jewerly.top().weight << " " << jewerly.top().value << endl;
-	jewerly.pop();
-	cout << jewerly.top().weight << " " << jewerly.top().value << endl;
-	jewerly.pop();
-	cout << jewerly.top().weight << " " << jewerly.top().value << endl;
-	jewerly.pop();
-	cout << jewerly.top().weight << " " << jewerly.top().value << endl;
-	jewerly.pop();
-	cout << jewerly.top().weight << " " << jewerly.top().value << endl;
-	jewerly.pop();*/
-
-	//vector<int> a = { 1, 2, 3, 4, 5 };
-	//cout << "lower bound : " << lower_bound(a.begin(), a.end(), 10) - a.begin() << endl;
-
-	/*multiset<int> a;
-	a.insert(1);
-	if (a.lower_bound(2) == a.end()) {
-		cout << "success" << endl;
-	}*/
 
 
 }
